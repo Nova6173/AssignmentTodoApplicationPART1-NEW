e368e6f985543d070bd6b2f7fbfbf8da24c6f58b
Jeanette S <nova777nova@hotmail.com>
2024-04-02 09:59:49 +0200

    renamed project to AssignmentTodoApplicationPART1&2 because i forgot to push nr 1 before i did nr 2

---
+++ src/main/java/AssignmentTodoApplicationPART1-NEW/src/main/java/se/JavaLexicon/assignmentsTodoApplication/AppRole.java
@@ -0,0 +1,6 @@
+package se.JavaLexicon.assignmentsTodoApplication;
+
+public enum AppRole {
+    ROLE_APP_USER,
+    ROLE_APP_ADMIN
+}
---
+++ src/main/java/AssignmentTodoApplicationPART1-NEW/src/main/java/se/JavaLexicon/assignmentsTodoApplication/AppUser.java
@@ -0,0 +1,67 @@
+package se.JavaLexicon.assignmentsTodoApplication;
+
+public class AppUser {
+    private String username;
+    private String password;
+    private AppRole role;
+
+    public AppUser(String username, String password, AppRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AppRole getRole() {
+        return role;
+    }
+
+    public void setRole(AppRole role) {
+        this.role = role;
+    }
+
+    // toString, equals, and hashCode methods
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "username='" + username + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AppUser appUser = (AppUser) o;
+
+        if (!username.equals(appUser.username)) return false;
+        return role == appUser.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+}
